c2b73aa95085ed7cc56c4aa994908e38e494442f
sarahliz24 <sarahliz24@hotmail.com>
2023-04-07 10:55:53 +0000

    Create welcome menu function

--- run.py
+++ run.py
@@ -45,4 +45,40 @@ def welcome():
     print('    - All staff and all skills in one table\n')
 
 
+def welcome_menu():
+    """
+    Gives 3 options to user: enter new staff member;
+        update staff member; search.  Sends user to
+        selected menu page after successful input.
+        Manages input errors.
+    """
+    print('MENU OPTIONS\n')
+    print('1: Enter a new staff member & add skills')
+    print('2: Update skills for an existing staff member')
+    print('3: Search records by skill, staff or all\n')
+
+    while True:
+        try:
+            answer = int(input('Enter 1, 2 or 3 to proceed (or 0 to exit):'))
+        except ValueError:
+            # if entering a letter or other non-number key return to input
+            print('please choose a valid option from the menu\n')
+            continue
+        if answer > 3:
+            #  if entering a number not 1-3 or 9, set to return to input
+            print('please choose a valid option from the menu\n')
+            continue
+        break
+    if answer == 1:
+        print('you answered one')
+    elif answer == 2:
+        print('you answered two')
+    elif answer == 3:
+        print('you answered three')
+    elif answer == 0:
+        print('you answered 0')
+    #  each option will send user to appropriate new menu/page (add function)
+
+
 welcome()
+welcome_menu()
