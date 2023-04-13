86f8971e02c7a63e12ac19a56056f39979b984aa
sarahliz24 <sarahliz24@hotmail.com>
2023-04-13 19:08:49 +0000

    Revert changes from last commit (made in error)

--- run.py
+++ run.py
@@ -319,6 +319,7 @@ def display_staff_menu():
     """
     print('\nDo you want to:\n')
     print('1: Search for another staff member')
+    print("2: Update this staff member's skills")
     print('0: Return to main menu\n')
 
     try:
@@ -327,12 +328,15 @@ def display_staff_menu():
         # if entering a letter or other non-number key return to input
         print('please choose a valid option from the menu\n')
     else:
-        if answer5 > 1:
+        if answer5 > 2:
             #  if entering a number not 1-3 or 9, set to return to input
             print('please choose a valid option from the menu\n')
     if answer5 == 1:
         print('you answered one')
         find_staff()
+    elif answer5 == 2:
+        print("This will be option 2")
+        # skill_menu(staff_entry)
     elif answer5 == 0:
         welcome_menu()
 
