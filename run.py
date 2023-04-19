37452fe2e47dc93e67293e8db36dc03a87e171bb
sarahliz24 <sarahliz24@hotmail.com>
2023-04-19 16:04:08 +0000

    Style: add green colorama to answer text in search_menu for consistency

--- run.py
+++ run.py
@@ -451,7 +451,7 @@ def search_menu():
             print('1: Staff search - displays all skills for a staff member')
             print('2: Skill search - displays all staff members with skill')
             print('0: Return to main menu\n')
-            answer = int(input('Enter 0, 1 or 2 to proceed:\n'))
+            answer = int(input(Fore.GREEN + 'Enter 0, 1 or 2 to proceed:\n'))
         except ValueError:
             # if entering a letter or other non-number key return to input
             print(Fore.RED +
